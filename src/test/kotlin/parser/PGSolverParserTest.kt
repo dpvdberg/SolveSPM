4b1ac4cba80b5e2fee87df203e9aa036825f997c
Dennis van den Berg <dennispvandenberg@gmail.com>
2020-03-15 15:34:31 +0100

    Add no header test for parsing

--- src/test/kotlin/parser/PGSolverParserTest.kt
+++ src/test/kotlin/parser/PGSolverParserTest.kt
@@ -40,4 +40,24 @@ internal class PGSolverParserTest {
         assert(node.priority == 7)
         assertNull(node.name)
     }
+
+    @Test
+    fun parseNoHeaderTest() {
+        val sample =
+            """
+            1 3 0 1,3,4 "Europe";
+            0 6 1 4,2 "Africa";
+            4 5 1 0 "Antarctica";
+            1 8 1 2,4,3 "America";
+            3 6 0 4,2 "Australia";
+            2 7 0 3,1,0,4;
+            """
+
+        val game = PGSolverParser.parse(sample)
+
+        assertNotNull(game);
+        assert(game is Game)
+        assertNull(game!!.id)
+        assert(game.nodes.size == 5)
+    }
 }
