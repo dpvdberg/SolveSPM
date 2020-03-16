bc56085bdf7c940b6a397d1fd45bd9bad3d4bfbb
Dennis van den Berg <dennispvandenberg@gmail.com>
2020-03-16 17:33:41 +0100

    WIP compareto

--- src/main/kotlin/evaluation/Measure.kt
+++ src/main/kotlin/evaluation/Measure.kt
@@ -1,8 +1,10 @@
 package evaluation
 
 import javax.xml.soap.Node
+import kotlin.math.abs
+import kotlin.math.pow
 
-sealed class Measure {
+sealed class Measure(var length: Int) {
     fun smallerUpTo(n: Measure, i: Int): Boolean {
         return false
     }
@@ -14,40 +16,54 @@ sealed class Measure {
 
 
     operator fun compareTo(pair: Pair<Measure, Int>): Int {
-        return -1
+        return MeasureUpToComparator.compare(Pair(this, pair.second), pair)
     }
 
     operator fun compareTo(other: Measure): Int {
-        return -1
+        return MeasureUpToComparator.compare(Pair(this, length), Pair(other, other.length))
     }
 }
 
-class MeasureComparator : Comparator<Measure> {
-    override fun compare(o1: Measure, o2: Measure): Int {
-        if (o1 is Loss) {
-            return if (o2 is Loss) {
-                0
-            } else {
-                1
+class MeasureUpToComparator {
+    companion object : Comparator<Pair<Measure, Int>> {
+        override fun compare(o1: Pair<Measure, Int>, o2: Pair<Measure, Int>): Int {
+            if (o1.first is Loss) {
+                return if (o2.first is Loss) {
+                    0
+                } else {
+                    1
+                }
+            } else if (o2.first is Loss) {
+                return -1
             }
-        } else if (o2 is Loss) {
-            return -1
+
+            val o1Tuple = o1.first as Tuple
+            val o2Tuple = o2.first as Tuple
+            return TupleComparator.compare(Pair(o1Tuple, o1.second), Pair(o2Tuple, o2.second))
         }
-
-        val o1Tuple = o1 as Tuple
-        val o2Tuple = o2 as Tuple
-        return o1Tuple.compareTo(o2Tuple)
     }
 }
 
-class TupleComparator : Comparator<Tuple> {
-    override fun compare(o1: Tuple, o2: Tuple): Int {
-        return -1
+class TupleComparator {
+    companion object : Comparator<Pair<Tuple, Int>>  {
+        override fun compare(o1: Pair<Tuple, Int>, o2: Pair<Tuple, Int>): Int {
+            if (o1.first.length != o2.first.length) {
+                throw Exception("Cannot compare Tuples of unequal size")
+            }
+
+            if (o1.second != o2.second) {
+                throw Exception("Cannot compare Tuples up to unequal length")
+            }
+
+            val differIndex = (0 until o1.second).firstOrNull { i -> o1.first.m[i] != o2.first.m[i]} ?: return 0
+
+            return 10.0.pow(differIndex).toInt() * (o1.first.m[differIndex] - o2.first.m[differIndex])
+        }
     }
 }
 
-class Tuple(length: Int) : Measure() {
+class Tuple(length : Int) : Measure(length) {
     var m = IntArray(length) { 0 }
 }
 
-class Loss : Measure()
+object Loss : Measure(0)
---
+++ src/test/kotlin/evaluation/MeasureTest.kt
@@ -0,0 +1,14 @@
+package evaluation
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class MeasureTest {
+
+    @org.junit.jupiter.api.Test
+    fun compareTo() {
+    }
+
+    @org.junit.jupiter.api.Test
+    fun testCompareTo() {
+    }
+}
